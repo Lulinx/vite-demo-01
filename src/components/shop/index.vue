7c761922fa831da9b3b64fc1d362e258f79ffeb1
Lulinx <Mr_Wangs@163.com>
2022-01-14 11:18:09 +0800

    格式化

--- src/components/shop/index.vue
+++ src/components/shop/index.vue
@@ -2,7 +2,7 @@
   <div class="page-shop">
     <van-loading class="loading" v-if="loading" type="spinner" />
     <van-card
-    v-else
+      v-else
       v-for="(items, index) in dataList"
       :key="index"
       num="2"
@@ -10,7 +10,7 @@
       :desc="items.engName"
       :title="items.name"
       :thumb="items.coverURL"
-       @click="goToDetail()"
+      @click="goToDetail()"
     />
   </div>
 </template>
@@ -19,7 +19,7 @@
 import { useRouter } from "vue-router";
 import { dogFamily } from "@/api/api";
 import { onMounted, reactive, ref, toRefs } from "@vue/runtime-core";
-import { Card,Loading } from "vant";
+import { Card, Loading } from "vant";
 import { getCurrentInstance } from "vue";
 export default {
   name: "shop",
@@ -36,9 +36,9 @@ export default {
         pageNum: 1,
         totalCount: 0,
       },
-      loading:true,
+      loading: true,
       keyword: "",
-      dataList:[]
+      dataList: [],
     });
     const goToDetail = () => {
       router.push({
@@ -52,16 +52,18 @@ export default {
         page: pageNum,
         pageSize: pageSize,
         keyword: "",
-      }).then((res) => {
-        if (res.statusCode == "000000") {
-          formData.dataList.push(...res.result.petFamilyList);
-          formData.loading = false;
-          console.log(formData.dataList);
-        }
-      }).catch((err) => {
+      })
+        .then((res) => {
+          if (res.statusCode == "000000") {
+            formData.dataList.push(...res.result.petFamilyList);
+            formData.loading = false;
+            console.log(formData.dataList);
+          }
+        })
+        .catch((err) => {
           console.log(err);
           proxy.$toast("网络异常");
-        });;
+        });
     });
     return {
       goToDetail,
