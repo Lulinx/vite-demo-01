46a8f35ea2e0130f0b481d816afc8a1317fbda64
Lulinx <Mr_Wangs@163.com>
2022-01-17 18:01:45 +0800

    封装精确小数计算的API

--- src/utils/extend.js
+++ src/utils/extend.js
@@ -1,5 +1,39 @@
+/*
+ * @Author: ws
+ * @Date: 2022-01-14 08:52:47
+ * @LastEditTime: 2022-01-17 18:00:10
+ * @LastEditors: ws
+ * @Description: 工具类API.js
+ * @FilePath: \myDemo\vite-demo-01\src\utils\extend.js
+ */
 import { Toast } from "vant";
+import { create, all } from 'mathjs'
+const config = {
+    epsilon: 1e-12,
+    matrix: 'Matrix',
+    number: 'BigNumber', // 可选值：number BigNumber
+    precision: 64,
+    predictable: false,
+    randomSeed: null
+}
+const math = create(all, config);
 
 export function showToast(message) {
   return Toast(message);
+}
+/**
+ * @description: 
+ * @param {Number} num1
+ * @param {Number} num2
+ * @param {String} symbol
+ * @return {String}
+ */
+ export function mathJsCalc([num1 = "", num2 = ""], symbol = "+") {
+  const MathCompMap = {
+      "+": math.format(math.add(math.bignumber(num1), math.bignumber(num2))), // 3.3
+      "-": math.format(math.subtract(math.bignumber(num1), math.bignumber(num2))),
+      "*": math.format(math.multiply(math.bignumber(num1), math.bignumber(num2))),
+      "/": math.format(math.divide(math.bignumber(num1), math.bignumber(num2))),
+  }
+  return MathCompMap[symbol];
 }
