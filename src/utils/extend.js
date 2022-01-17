272856926aa9519b1ed394645e0358b272312fa0
Lulinx <Mr_WangS@163.com>
2022-01-17 20:20:31 +0800

    增加格式化日期各种日期格式的公共API

--- src/utils/extend.js
+++ src/utils/extend.js
@@ -1,13 +1,14 @@
 /*
  * @Author: ws
  * @Date: 2022-01-14 08:52:47
- * @LastEditTime: 2022-01-17 18:00:10
+ * @LastEditTime: 2022-01-17 20:19:31
  * @LastEditors: ws
  * @Description: 工具类API.js
- * @FilePath: \myDemo\vite-demo-01\src\utils\extend.js
+ * @FilePath: \vite-demo-pce:\code\vue\vite-demo-01\src\utils\extend.js
  */
 import { Toast } from "vant";
 import { create, all } from 'mathjs'
+import moment from "moment";
 const config = {
     epsilon: 1e-12,
     matrix: 'Matrix',
@@ -18,11 +19,16 @@ const config = {
 }
 const math = create(all, config);
 
+/**
+ * @description: 提示toast
+ * @param {String} message --提示内容
+ * @return {Vnode} Toast
+ */
 export function showToast(message) {
   return Toast(message);
 }
 /**
- * @description: 
+ * @description: 解决小数计算不精确的问题
  * @param {Number} num1
  * @param {Number} num2
  * @param {String} symbol
@@ -30,10 +36,20 @@ export function showToast(message) {
  */
  export function mathJsCalc([num1 = "", num2 = ""], symbol = "+") {
   const MathCompMap = {
-      "+": math.format(math.add(math.bignumber(num1), math.bignumber(num2))), // 3.3
+      "+": math.format(math.add(math.bignumber(num1), math.bignumber(num2))),
       "-": math.format(math.subtract(math.bignumber(num1), math.bignumber(num2))),
       "*": math.format(math.multiply(math.bignumber(num1), math.bignumber(num2))),
       "/": math.format(math.divide(math.bignumber(num1), math.bignumber(num2))),
   }
   return MathCompMap[symbol];
+}
+
+/**
+ * @description: 
+ * @param {String} current --当前时间
+ * @param {String} dateType --格式化日期格式("YYYY-MM-DD HH:mm:ss")
+ * @return {String}
+ */
+export function dateFormat(current, dateType){
+  return moment(current).format(dateType);
 }
